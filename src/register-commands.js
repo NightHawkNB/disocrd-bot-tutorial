88ad9480d85549aa278b7f83ede829e024fd0374
NightHawkNB <nipunbathiya1256@outlook.com>
2023-07-17 05:37:27 +0530

    Registering options and choices for the slash commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,14 +1,38 @@
 require('dotenv').config()
-const { REST, Routes } = require('discord.js')
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
 
 const commands = [
     {
-        name: 'hey',
-        description: 'Replies with hey!'
-    },
-    {
-        name: 'ping',
-        description: 'Pong !'
+        name: 'add',
+        description: 'Add two numbers',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1
+                    },
+                    {
+                        name: 'two',
+                        value: 2
+                    },
+                    {
+                        name: 'five',
+                        value: 5
+                    }
+                ],
+                required: true
+            },
+            {
+                name: 'second-number',
+                description: 'The second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            }
+        ]
     }
 ]
 
