fdf70c5001e11cea9d6afc2fa3e70655fdad0358
NightHawkNB <nipunbathiya1256@outlook.com>
2023-07-18 05:26:58 +0530

    Role Selection using a Button - Next : Bot Presence/Status

--- src/index.js
+++ src/index.js
@@ -14,55 +14,84 @@ client.on('ready', (c) => {
     console.log(`✅ ${c.user.tag} is ready`)
 })
 
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        if (interaction.commandName === 'add') {
+            const num1 = interaction.options.get('first-number').value
 
-    if (interaction.commandName === 'add') {
-        const num1 = interaction.options.get('first-number').value
+            //! Here ? infront of .value means an optional value
+            //* const num2 = interaction.options.get('second-number')?.value
+            const num2 = interaction.options.get('second-number').value
 
-        //! Here ? infront of .value means an optional value
-        //* const num2 = interaction.options.get('second-number')?.value
-        const num2 = interaction.options.get('second-number').value
+            interaction.reply(`The sum is ${num1+num2 }`)
+        }else if(interaction.commandName === 'embed'){
+            const embed = new EmbedBuilder()
+                .setTitle("Embed title")
+                .setDescription('This is an Embed')
+                .setColor('Random')
+                .addFields(
+                    {
+                        name: `:smile: Field 1`,
+                        value: 'Some random value',
+                        inline: true
+                    },
+                    {
+                        name: ':grin: Field 2',
+                        value: 'Any value',
+                        inline: true
+                    },
+                    {
+                        name: ':smiley: Field 3',
+                        value: 'Field 3 Values',
+                        inline: true
+                    },
+                    {
+                        name: 'Field 1',
+                        value: 'Some random value',
+                        inline: true
+                    },
+                    {
+                        name: 'Field 2',
+                        value: 'Any value',
+                        inline: true
+                    },
+                    {
+                        name: 'Field 3',
+                        value: 'Field 3 Values',
+                        inline: true
+                    }
+                )
+            interaction.reply({embeds: [embed]})
+        }
+    }
 
-        interaction.reply(`The sum is ${num1+num2 }`)
-    }else if(interaction.commandName === 'embed'){
-        const embed = new EmbedBuilder()
-            .setTitle("Embed title")
-            .setDescription('This is an Embed')
-            .setColor('Random')
-            .addFields(
-                {
-                    name: `:smile: Field 1`,
-                    value: 'Some random value',
-                    inline: true
-                },
-                {
-                    name: ':grin: Field 2',
-                    value: 'Any value',
-                    inline: true
-                },
-                {
-                    name: ':smiley: Field 3',
-                    value: 'Field 3 Values',
-                    inline: true
-                },
-                {
-                    name: 'Field 1',
-                    value: 'Some random value',
-                    inline: true
-                },
-                {
-                    name: 'Field 2',
-                    value: 'Any value',
-                    inline: true
-                },
-                {
-                    name: 'Field 3',
-                    value: 'Field 3 Values',
-                    inline: true
-                }
-            )
-        interaction.reply({embeds: [embed]})
+
+    //? The interactions for selecting role buttons
+    try {
+        if(!interaction.isButton()) return;
+        else {
+            await interaction.deferReply({ephermeral : true})
+
+            const role = interaction.guild.roles.cache.get(interaction.customId)
+            if(!role) {
+                interaction.reply({
+                    content: "I cound't find the role",
+                })
+                return;
+            }
+
+            const hasRole = interaction.member.roles.cache.has(role.id)
+            if(hasRole){
+                await interaction.member.roles.remove(role)
+                await interaction.editReply(`The role ${role} has been removed.`)
+                return;
+            }
+
+            await interaction.member.roles.add(role)
+            await interaction.editReply(`The role ${role} has been added.`)
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 
